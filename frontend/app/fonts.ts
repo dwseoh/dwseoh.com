0f433306a2e09426036869d1dfbe74df97aff34b
Dongwan Seoh <dongwanseoh@gmail.com>
2026-07-01 20:15:11 -0400

    fix: explicit korean font declaration

--- frontend/app/fonts.ts
+++ frontend/app/fonts.ts
@@ -1,4 +1,4 @@
 import { Inter, Gowun_Dodum } from 'next/font/google'
 
 export const inter = Inter({ subsets: ['latin'] })
-export const gowunDodum = Gowun_Dodum({ weight: '400', subsets: ['latin'] })
+export const gowunDodum = Gowun_Dodum({ weight: '400', subsets: ['latin', 'korean'] })
